e94553b2cf005f5257f9c66600cfe96fccec79ed
Kaique José de Souza Santos <kaique30122004@gmail.com>
2022-03-19 23:15:07 -0300

    soma total corrigida

--- Cliente/_js/compracliente.js
+++ Cliente/_js/compracliente.js
@@ -7,14 +7,12 @@ function adicionar_AUX(visor,id) {
     document.getElementById(visor).value = valor + 1;
     var p = produtos[id];
     var item = `<tr class="tabela-linha"><strong>Nome: </strong>${p.nome}&nbsp;<strong>Id: </strong>${p.id}`+(valor+1)+`&nbsp;<strong>Preço R$: </strong>${p.preço}</td></tr>`;
-    console.log(valor);
     if (valor==0) {
-        tabela.innerHTML = tabela.innerHTML + aux+item;
-        calc(codigo);
+        tabela.innerHTML = tabela.innerHTML +item;
+        calc(id+1);
     }else{
         aux.innerHTML = tabela.innerHTML + item;
-        calc(codigo);
-        valor++;
+        calc(id+1);
     }
 }
 
@@ -28,7 +26,6 @@ function adicionar(codigo) {
     }
     if (codigo == 3) {
         adicionar_AUX("visor3", 2)
-
     }
     if (codigo == 4) {
         adicionar_AUX("visor4", 3)
@@ -49,110 +46,95 @@ function adicionar(codigo) {
         adicionar_AUX("visor9", 8)
     }
 }
-
-function remover(codigo) {
-    if (codigo == 1 && document.getElementById('visor1').value>0) {
-        valor = Number(document.getElementById('visor1').value);
-        document.getElementById('visor1').value = valor - 1;
+function remover_AUX(id,visor) {
+    if (document.getElementById(visor).value>0) {
+        valor = Number(document.getElementById(visor).value);
+        document.getElementById(visor).value = valor - 1;
         var tabela = document.getElementById('tabela');
-        var item = `<tr class="tabela-linha"><strong>Nome: </strong>Macarrão&nbsp;<strong>Id: </strong> 0010`+(valor-1)+`&nbsp;<strong>Preço R$: </strong> 2,4</tr>`;
-            if (document.getElementById('visor1').value<1) {
+        var p = produtos[id-1];
+        var item = `<tr class="tabela-linha"><strong>Nome: </strong>${p.nome}&nbsp;<strong>Id: </strong>${p.id}`+(valor-1)+`&nbsp;<strong>Preço R$: </strong>${p.preço}</td></tr>`;
+            if (document.getElementById(visor).value<1) {
                 item = `<tr class="tabela-linha"><td class="tabela-texto"></td></tr>`;
                 tabela.innerHTML = item;
-                calc(codigo);
+                calc(id);
             }else{
                 tabela.innerHTML = item;
-                calc(codigo);
+                calc(id);
                 }
     }
-    if (codigo == 2 && document.getElementById('visor2').value>0) {
-        valor = Number(document.getElementById('visor2').value);
-        document.getElementById('visor2').value = valor - 1;
-        calc(codigo);
+}
+
+function remover(codigo) {
+    if (codigo == 1) {
+        remover_AUX(codigo,"visor1");
     }
-    if (codigo == 3 && document.getElementById('visor3').value>0) {
-        valor = Number(document.getElementById('visor3').value);
-        document.getElementById('visor3').value = valor - 1;
-        calc(codigo);
+    if (codigo == 2) {
+        remover_AUX(codigo,"visor2");
     }
-    if (codigo == 4 && document.getElementById('visor4').value>0) {
-        valor = Number(document.getElementById('visor4').value);
-        document.getElementById('visor4').value = valor - 1;
-        calc(codigo);
+    if (codigo == 3) {
+        remover_AUX(codigo,"visor3");
     }
-    if (codigo == 5 && document.getElementById('visor5').value>0) {
-        valor = Number(document.getElementById('visor5').value);
-        document.getElementById('visor5').value = valor - 1;
-        calc(codigo);
+    if (codigo == 4) {
+        remover_AUX(codigo,"visor4");
     }
-    if (codigo == 6 && document.getElementById('visor6').value>0) {
-        valor = Number(document.getElementById('visor6').value);
-        document.getElementById('visor6').value = valor - 1;
-        calc(codigo);
+    if (codigo == 5) {
+        remover_AUX(codigo,"visor5");
     }
-    if (codigo == 7 && document.getElementById('visor7').value>0) {
-        valor = Number(document.getElementById('visor7').value);
-        document.getElementById('visor7').value = valor - 1;
-        calc(codigo);
+    if (codigo == 6) {
+        remover_AUX(codigo,"visor6");
     }
-    if (codigo == 8 && document.getElementById('visor8').value>0) {
-        valor = Number(document.getElementById('visor8').value);
-        document.getElementById('visor8').value = valor - 1;
-        calc(codigo);
+    if (codigo == 7) {
+        remover_AUX(codigo,"visor7");
     }
-    if (codigo == 9 && document.getElementById('visor9').value>0) {
-        valor = Number(document.getElementById('visor9').value);
-        document.getElementById('visor9').value = valor - 1;
-        calc(codigo);
+    if (codigo == 8) {
+        remover_AUX(codigo,"visor8");
+    }
+    if (codigo == 9) {
+        remover_AUX(codigo,"visor9");
     }
 }
 
 function voltar() {
     location.href = "../_html/logincliente.html"
 }
-bank[9]
-caixa = 0;
 
-function cal_AUX(codigo,id,visor,produto) {
-    if (codigo==id) {
+var bank = [0,0,0,0,0,0,0,0,0];
+var caixa = 0;
+
+function cal_AUX(id,visor,produto) {
         valordovisor = Number(document.getElementById(visor).value);
-        resultado = valordovisor * produto.preço;
+        resultado = valordovisor * produto;
         bank[id-1] = resultado;
-        console.log(resultado)
    }
-}
 
 function calc(codigo) {
-    cal_AUX(codigo,1,"visor1",macarrao)
-       
-   
+    if (codigo==1) {
+        cal_AUX(codigo,"visor1",macarrao.preço)
+    }  
    if (codigo==2) {
-    cal_AUX(codigo,"visor2", arroz)
+    cal_AUX(codigo,"visor2", arroz.preço)
    }
    if (codigo==3) {
-    cal_AUX(codigo,"visor3",feijao)
+    cal_AUX(codigo,"visor3",feijao.preço)
     }
     if (codigo==4) {
-        cal_AUX(codigo,"visor4",carne)
+        cal_AUX(codigo,"visor4",carne.preço)
     }
     if (codigo==5) {
-        cal_AUX(codigo,"visor5",frango)
+        cal_AUX(codigo,"visor5",frango.preço)
     }
     if (codigo==6) {
-        cal_AUX(codigo,"visor6",peixe)
+        cal_AUX(codigo,"visor6",peixe.preço)
     }
     if (codigo==7) {
-        cal_AUX(codigo,"visor7",banana)
+        cal_AUX(codigo,"visor7",banana.preço)
     }
     if (codigo==8) {
-        cal_AUX(codigo,"visor8",maca)
+        cal_AUX(codigo,"visor8",maca.preço)
     }
     if (codigo==9) {
-        cal_AUX(codigo,"visor9",tomate)
+        cal_AUX(codigo,"visor9",tomate.preço)
     }
-   
-   for (let index = 0; index < bank.length; index++) {
-       caixa+= bank[index]
-   }
-   document.getElementById('visorfinal').value = (caixa).toFixed(2);
+    caixa = bank[0]+bank[1]+bank[2]+bank[3]+bank[4]+bank[5]+bank[6]+bank[7]+bank[8];
+    document.getElementById('visorfinal').value = (caixa).toFixed(2);
 }
